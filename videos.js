35b7336332ae4f95efee7d6a55c62ca06e62e117
Bruno Di Giuseppe <Bruno Di Giuseppe>
2016-10-22 19:32:34 -0200

    fineshed concept and added some videos. Could add some more.

---
+++ videos.js
@@ -0,0 +1,44 @@
+function GetVideos(){
+	var videoUrls = [];
+
+	videoUrls.push("videos/mini/1.mp4");
+	videoUrls.push("videos/mini/2.mp4");
+	videoUrls.push("videos/mini/3.mp4");
+	videoUrls.push("videos/mini/4.mp4");
+	videoUrls.push("videos/mini/5.mp4");
+	videoUrls.push("videos/mini/6.mp4");
+	videoUrls.push("videos/mini/7.mp4");
+	videoUrls.push("videos/mini/8.mp4");
+	videoUrls.push("videos/mini/9.mp4");
+	videoUrls.push("videos/mini/10.mp4");
+	videoUrls.push("videos/mini/11.mp4");
+	videoUrls.push("videos/mini/12.mp4");
+	videoUrls.push("videos/mini/13.mp4");
+	videoUrls.push("videos/mini/14.mp4");
+	videoUrls.push("videos/mini/15.mp4");
+	videoUrls.push("videos/mini/16.mp4");
+	videoUrls.push("videos/mini/17.mp4");
+	videoUrls.push("videos/mini/18.mp4");
+	videoUrls.push("videos/mini/19.mp4");
+	videoUrls.push("videos/mini/20.mp4");
+	videoUrls.push("videos/mini/21.mp4");
+	videoUrls.push("videos/mini/22.mp4");
+	videoUrls.push("videos/mini/23.mp4");
+	videoUrls.push("videos/mini/24.mp4");
+	videoUrls.push("videos/mini/25.mp4");
+	videoUrls.push("videos/mini/26.mp4");
+	videoUrls.push("videos/mini/27.mp4");
+	videoUrls.push("videos/mini/28.mp4");
+	videoUrls.push("videos/mini/29.mp4");
+	videoUrls.push("videos/mini/30.mp4");
+	videoUrls.push("videos/mini/31.mp4");
+	videoUrls.push("videos/mini/32.mp4");
+	videoUrls.push("videos/mini/33.mp4");
+	videoUrls.push("videos/mini/34.mp4");
+	videoUrls.push("videos/mini/35.mp4");
+	videoUrls.push("videos/mini/36.mp4");
+	videoUrls.push("videos/mini/37.mp4");
+	videoUrls.push("videos/mini/38.mp4");
+
+	return videoUrls;
+}
